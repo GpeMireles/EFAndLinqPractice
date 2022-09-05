467c03121aa1a3118ae8cf161664f71c80277c3f
Jose Guadalupe Mireles Rosales <jose-guadalupe.mirelesrosales.external@atos.net>
2022-09-05 13:12:56 -0500

    Db Context

--- src/Models/SchoolContext.cs
+++ src/Models/SchoolContext.cs
@@ -2,8 +2,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace EFAndLinqPractice_SchoolAPI.Models
 {
-    public class SchoolContext
+    public class SchoolContext : DbContext
     {
-        
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
     }
 }
